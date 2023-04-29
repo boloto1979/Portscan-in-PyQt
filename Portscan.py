760dc39fd8752d7f51974d4a7a701517ab1d9b8f
Pedro Lima <34582475+boloto1979@users.noreply.github.com>
2023-04-29 01:27:39 -0300

    Update Portscan.py
    1. Importing the QFrame module:
    from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QPushButton, QPlainTextEdit, QFrame
    I imported the QFrame class to create a horizontal line to visually separate the fields.
    2. Adding the horizontal line:
    I created a QFrame instance called line and we positioned it below the IP input field. We set its geometry to occupy a width of 380 pixels and a height of 2 pixels, creating a horizontal line. setFrameShape(QFrame.HLine) sets the line format to horizontal and setFrameShadow(QFrame.Sunken) sets a shadow effect on the line.
    3. Adjust the position of the scan time field:
    I moved the scan time entry field down below the horizontal line to create a visual space separating it from the IP field.

--- Portscan.py
+++ Portscan.py
@@ -1,7 +1,10 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QPushButton, QPlainTextEdit
+import asyncio
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QPushButton, QPlainTextEdit, QFrame
 from PyQt5.QtCore import Qt
+import concurrent.futures
 import socket
+import subprocess
 
 class PortScan(QWidget):
     def __init__(self):
@@ -17,15 +20,26 @@ class PortScan(QWidget):
         self.host_input = QLineEdit(self)
         self.host_input.move(100, 10)
 
+        self.line = QFrame(self)
+        self.line.setGeometry(10, 40, 380, 2)
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+
+        self.timeout_label = QLabel("Timeout (seconds):", self)
+        self.timeout_label.move(10, 60)
+
+        self.timeout_input = QLineEdit(self)
+        self.timeout_input.move(150, 60)
+
         self.result_label = QLabel("Result:", self)
-        self.result_label.move(10, 50)
+        self.result_label.move(10, 90)
 
         self.result_output = QPlainTextEdit(self)
-        self.result_output.move(10, 70)
+        self.result_output.move(10, 110)
         self.result_output.setReadOnly(True)
 
         self.scan_button = QPushButton("Scan", self)
-        self.scan_button.move(10, 250)
+        self.scan_button.move(300, 250)
         self.scan_button.clicked.connect(self.start_scan)
 
         self.setGeometry(300, 300, 400, 300)
@@ -33,20 +47,56 @@ class PortScan(QWidget):
 
     def start_scan(self):
         host = self.host_input.text()
+        timeout = float(self.timeout_input.text())
         ports = [21, 22, 23, 25, 53, 80, 110, 143, 443, 587, 993, 995]
 
         self.result_output.clear()
 
-        for port in ports:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(0.5)
-            result = sock.connect_ex((host, port))
-            sock.close()
+        asyncio.run(self.scan_async(host, ports, timeout))
 
+    async def scan_async(self, host, ports, timeout):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            loop = asyncio.get_event_loop()
+            tasks = [
+                loop.run_in_executor(executor, self.scan_port, host, port, timeout)
+                for port in ports
+            ]
+            port_results = await asyncio.gather(*tasks)
+
+        ping_result = await self.ping(host)
+
+        self.process_results(ports, port_results, ping_result)
+
+    def scan_port(self, host, port, timeout):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(timeout)
+        result = sock.connect_ex((host, port))
+        sock.close()
+        return result
+
+    async def ping(self, host):
+        try:
+            process = await asyncio.create_subprocess_shell(
+                f"ping -c 1 {host}",
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            _, _ = await process.communicate()
+            return process.returncode == 0
+        except Exception:
+            return False
+
+    def process_results(self, ports, port_results, ping_result):
+        for port, result in zip(ports, port_results):
             if result == 0:
-                self.result_output.appendPlainText("Port {} is open".format(port))
+                self.result_output.appendPlainText(f"Port {port} is open")
             else:
-                self.result_output.appendPlainText("Port {} is closed".format(port))
+                self.result_output.appendPlainText(f"Port {port} is closed")
+
+        if ping_result:
+            self.result_output.appendPlainText("Ping successful")
+        else:
+            self.result_output.appendPlainText("Ping failed")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
